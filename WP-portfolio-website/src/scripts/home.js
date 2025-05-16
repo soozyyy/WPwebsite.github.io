290af9bd97827639cee81ba6ca10c3ebb12a59de
Soo Zhen Yang <148412780+soozyyy@users.noreply.github.com>
2025-05-16 08:31:12 +0800

    fix CSS RWD

---
+++ WP-portfolio-website/src/scripts/home.js
@@ -0,0 +1,31 @@
+export function homePage() {
+    let home = `
+        <div class="home-content">
+            <div class="content-box">
+                <div class="box-image">
+                    <img src="./images/web-project.png" alt="vision">
+                </div>
+                <div class="box-content">
+                    <h2 >Vision</h2>
+                    <p>To inspire innovation and collaboration through meaningful web development projects that make a real-world impact.
+                </p>
+                </div>
+            </div>
+            
+            <div class="content-box content-box-2">
+                <div class="box-image">
+                    <img src="./images/mission.jpg" alt="mission">
+                </div>
+                <div class="box-content">
+                    <h2 >Mission</h2>
+                    <p class="desc-2">Our mission is to apply our web programming skills to develop practical, user-friendly solutions while fostering teamwork, creativity, and continuous learning throughout every project. We aim to contribute meaningfully to the tech community by creating open, accessible, and well-crafted web applications.</p>
+                </div>
+            </div>
+        </div>
+    `;
+    
+    document.querySelector('.js-home').innerHTML = home;
+
+    return home;
+}
+homePage();
